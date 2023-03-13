357180070d898b7efa28f6549a6d026db72ba93e
Barbara Klimeková <baskaklimek@Barbaras-MacBook-Pro.local>
2023-03-13 21:50:52 +0100

    using spring initializr for initialize the project!

---
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/BiotechApplication.java
@@ -0,0 +1,13 @@
+package cz.utb.bachelor.thesis.klimekova.biotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiotechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiotechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/utb/bachelor/thesis/klimekova/biotech/BiotechApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.utb.bachelor.thesis.klimekova.biotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiotechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
