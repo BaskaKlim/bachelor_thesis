40ccc1e5947f85dfdf397177587fda535b3525d0
baskaklimek <baska.klimekova@gmail.com>
2023-03-15 11:31:02 +0100

    add expert model

---
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/BiotechExpert.java
@@ -0,0 +1,18 @@
+package cz.utb.bachelor.thesis.klimekova.biotech.model;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class BiotechExpert {
+
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String jobPosition;
+    private String email;
+    private String linkedinUrl;
+    private String backgroundDescription;
+    private Set<ExpertCategory> expertise = new HashSet<>();
+}
