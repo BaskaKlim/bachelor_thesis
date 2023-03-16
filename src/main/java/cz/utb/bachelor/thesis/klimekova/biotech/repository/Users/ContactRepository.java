30810733bc4bc965f35d90a28ab1a2dff5be7427
baskaklimek <baska.klimekova@gmail.com>
2023-03-16 19:31:42 +0100

    add ContactRepository

---
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/repository/Users/ContactRepository.java
@@ -0,0 +1,13 @@
+package cz.utb.bachelor.thesis.klimekova.biotech.repository.Users;
+
+
+import cz.utb.bachelor.thesis.klimekova.biotech.model.Users.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, UUID> {
+
+}
