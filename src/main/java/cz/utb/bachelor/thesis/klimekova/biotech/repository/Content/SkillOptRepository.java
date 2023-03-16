cf1f7196296770a8c316b1fec3122482c0ed1114
baskaklimek <baska.klimekova@gmail.com>
2023-03-16 19:26:14 +0100

    add SkillOptRepository

---
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/repository/Content/SkillOptRepository.java
@@ -0,0 +1,13 @@
+package cz.utb.bachelor.thesis.klimekova.biotech.repository.Content;
+
+
+import cz.utb.bachelor.thesis.klimekova.biotech.model.Content.SkillOpt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SkillOptRepository extends JpaRepository<SkillOpt, UUID> {
+
+}
