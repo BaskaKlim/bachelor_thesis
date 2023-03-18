bda264ba10938968972c042ef25d614e3060bc58
baskaklimek <baska.klimekova@gmail.com>
2023-03-18 22:20:00 +0100

    add Service for NewsletterLead

---
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/serivce/Users/NewsletterLeadService.java
@@ -0,0 +1,46 @@
+package cz.utb.bachelor.thesis.klimekova.biotech.serivce.Users;
+
+import cz.utb.bachelor.thesis.klimekova.biotech.model.Users.NewsletterLead;
+import cz.utb.bachelor.thesis.klimekova.biotech.repository.Users.NewsletterLeadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class NewsletterLeadService  {
+
+    NewsletterLeadRepository newsletterLeadRepository;
+
+    @Autowired
+    public NewsletterLeadService (NewsletterLeadRepository newsletterLeadRepository) {
+        this.newsletterLeadRepository = newsletterLeadRepository;
+    }
+
+    public Optional<NewsletterLead> getNewsletterLeadById(UUID id) {
+        return newsletterLeadRepository.findById(id);
+    }
+
+    public NewsletterLead addNewsletterLead(NewsletterLead newsletterLead) {
+        return  newsletterLeadRepository.save(newsletterLead);
+
+    }
+
+    public void deleteNewsletterLeadById(UUID id) {
+        newsletterLeadRepository.deleteById(id);
+    }
+
+    public NewsletterLead updateNewsletterLead(NewsletterLead newsletterLead) {
+        return newsletterLeadRepository.save(newsletterLead);
+    }
+    public List<NewsletterLead> getAllNewsletterLead() {
+        List<NewsletterLead> newsletterLeads = new ArrayList<NewsletterLead>();
+        newsletterLeadRepository.findAll().forEach(newsletterLeads::add);
+
+        return newsletterLeads;
+    }
+
+}
