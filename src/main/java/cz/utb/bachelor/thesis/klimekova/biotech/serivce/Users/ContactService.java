45400e1b10bbac37506f35d81e7f04772aca0960
baskaklimek <baska.klimekova@gmail.com>
2023-03-18 21:56:57 +0100

    add Service for Contact

---
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/serivce/Users/ContactService.java
@@ -0,0 +1,44 @@
+package cz.utb.bachelor.thesis.klimekova.biotech.serivce.Users;
+
+import cz.utb.bachelor.thesis.klimekova.biotech.model.Users.Contact;
+import cz.utb.bachelor.thesis.klimekova.biotech.repository.Users.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ContactService  {
+    ContactRepository contactRepository;
+
+    @Autowired
+    public ContactService (ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    public Optional<Contact> getContactById(UUID id) {
+        return contactRepository.findById(id);
+    }
+
+    public Contact addContact(Contact account) {
+        return  contactRepository.save(account);
+
+    }
+
+    public void deleteContactById(UUID id) {
+        contactRepository.deleteById(id);
+    }
+
+    public Contact updateContact(Contact account) {
+        return contactRepository.save(account);
+    }
+    public List<Contact> getAllContact() {
+        List<Contact> accounts = new ArrayList<Contact>();
+        contactRepository.findAll().forEach(accounts::add);
+
+        return accounts;
+    }
+}
