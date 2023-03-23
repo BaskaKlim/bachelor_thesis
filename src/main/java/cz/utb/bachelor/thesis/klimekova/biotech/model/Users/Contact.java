34f79ef43239290208b36c353354d33aae3393ea
baskaklimek <baska.klimekova@gmail.com>
2023-03-23 19:33:03 +0100

    add validation annotation for Contact.java

--- src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Users/Contact.java
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Users/Contact.java
@@ -1,6 +1,9 @@
 package cz.utb.bachelor.thesis.klimekova.biotech.model.Users;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,30 +11,40 @@ import lombok.Setter;
 
 import java.util.UUID;
 
+@Table(name = "Contacts")
 @Entity
-@Table(name = "contacts")
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Contact {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private UUID id;
-   private String firstName;
-   private String lastName;
-   private String phone;
-   private String email;
-   private String jobPosition;
-   private UUID accountUuid;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
 
-   public Contact(String firstName, String lastName, String phone, String email, String jobPosition, UUID accountUuid) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.phone = phone;
-      this.email = email;
-      this.jobPosition = jobPosition;
-      this.accountUuid = accountUuid;
-   }
+    @NotBlank(message = "First is mandatory")
+    @Size(min = 2, max = 15)
+    private String firstName;
+    @NotBlank(message = "Last name is mandatory")
+    @Size(min = 2, max = 15)
+    private String lastName;
+    @NotBlank(message = "Phone is mandatory")
+    private String phone;
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email is need to be in correct format")
+    private String email;
+
+    @NotBlank(message = "Job position is mandatory")
+    private String jobPosition;
+    private UUID accountUuid;
+
+    public Contact(String firstName, String lastName, String phone, String email, String jobPosition, UUID accountUuid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.jobPosition = jobPosition;
+        this.accountUuid = accountUuid;
+    }
 }
