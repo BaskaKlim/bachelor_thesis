e26c5afcd40efb3889e23c34d18e6a8464524b24
baskaklimek <baska.klimekova@gmail.com>
2023-03-24 21:33:13 +0100

    update annotation of ContentType

--- src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Content/ContentType.java
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Content/ContentType.java
@@ -1,9 +1,25 @@
 package cz.utb.bachelor.thesis.klimekova.biotech.model.Content;
 
 import cz.utb.bachelor.thesis.klimekova.biotech.utils.enums.EContentType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "ContentTypes")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContentType {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
     private EContentType name;
 }
