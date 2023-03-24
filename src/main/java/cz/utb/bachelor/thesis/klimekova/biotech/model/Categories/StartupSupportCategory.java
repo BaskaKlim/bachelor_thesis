a5606d7b1af6158fe7e6a270806c9e906bec803e
baskaklimek <baska.klimekova@gmail.com>
2023-03-24 20:34:37 +0100

    update validation for StartupSupportCategory.java

--- src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Categories/StartupSupportCategory.java
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Categories/StartupSupportCategory.java
@@ -1,8 +1,26 @@
 package cz.utb.bachelor.thesis.klimekova.biotech.model.Categories;
 
-import cz.utb.bachelor.thesis.klimekova.biotech.utils.enums.ESkillCategory;
+import cz.utb.bachelor.thesis.klimekova.biotech.utils.enums.ESupportCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+@Entity
+@Table(name = "SupportCategories")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StartupSupportCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    private ESkillCategory name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ESupportCategory name;
 }
