02575407afd812303117d4ce9040f62137b323b9
baskaklimek <baska.klimekova@gmail.com>
2023-03-25 20:53:24 +0100

    update validation for StartupOpt.java

--- src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Content/StartupOpt.java
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/model/Content/StartupOpt.java
@@ -2,20 +2,53 @@ package cz.utb.bachelor.thesis.klimekova.biotech.model.Content;
 
 import cz.utb.bachelor.thesis.klimekova.biotech.model.Categories.BiotechCategory;
 import cz.utb.bachelor.thesis.klimekova.biotech.model.Categories.StartupSupportCategory;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@Entity
+@Table(name = "StartupOpportunities")
+@Setter
+@Getter
+@NoArgsConstructor
 public class StartupOpt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
+    @NotBlank(message = "Title of startup opportunity is mandatory")
     private String title;
+
+    @NotBlank(message = "Provider of startup opportunity  is mandatory")
     private String provider;
+
+    @NotBlank(message = "Description of startup opportunity or service is mandatory")
     private String description;
+
+    @NotBlank(message = "Start date of startup opportunity or service is mandatory")
     private Date startDate;
+
+    @NotBlank(message = "End date of startup opportunity or service is mandatory")
     private Date endDate;
+
+    @NotBlank(message = "Website of startup opportunity or service is mandatory")
     private String website;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "startupOpt_biotechCategories",
+            joinColumns = @JoinColumn(name = "startupOpt_id"),
+            inverseJoinColumns = @JoinColumn(name = "biotechCategory_id"))
     private Set<BiotechCategory> categories = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "startupOpt_supportCategories",
+            joinColumns = @JoinColumn(name = "startupOpt_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<StartupSupportCategory> supportCategories = new HashSet<>();
 }
