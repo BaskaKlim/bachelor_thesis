35d6239c67bd0b4939e04002f5859375a7b44cea
baskaklimek <baska.klimekova@gmail.com>
2023-03-30 23:36:35 +0200

    updating AccountRepository - add existingByUsername and ByPassword methods

--- src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/repository/Users/AccountRepository.java
+++ src/main/java/cz/utb/bachelor/thesis/klimekova/biotech/repository/Users/AccountRepository.java
@@ -5,9 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+
+    Optional<Account> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByPassword(String password);
+
 
 }
