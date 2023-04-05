c46a4bfa72bb0b64fff89322a57c82e1b0d6779e
baskaklimek <baska.klimekova@gmail.com>
2023-04-05 18:45:53 +0200

    create enums categories for biotech

---
+++ howtodobiotech/src/main/java/cz/utb/fai/howtodobiotech/utils/enums/EBiotechCategory.java
@@ -0,0 +1,11 @@
+package cz.utb.fai.howtodobiotech.utils.enums;
+
+public enum EBiotechCategory {
+
+    Medicine_RED,
+    Environment_GREEN,
+    Industry_WHITE,
+    Food_YELLOW,
+    Marine_BLUE
+
+}
