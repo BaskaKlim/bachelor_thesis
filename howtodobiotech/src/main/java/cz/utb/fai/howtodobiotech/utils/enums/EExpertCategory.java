a0c8d49277cc7fc4bbba44c7ab329b44cb978261
baskaklimek <baska.klimekova@gmail.com>
2023-04-05 18:46:28 +0200

    create enums categories for experts

---
+++ howtodobiotech/src/main/java/cz/utb/fai/howtodobiotech/utils/enums/EExpertCategory.java
@@ -0,0 +1,12 @@
+package cz.utb.fai.howtodobiotech.utils.enums;
+
+public enum EExpertCategory {
+    BUSINESS_DEVELOPMENT,
+    LIFE_SCIENCE,
+    CHEMISTRY,
+    BIOLOGY,
+    BIOINFORMATICS,
+    DATA_SCIENCE,
+    LEGAL
+
+}
