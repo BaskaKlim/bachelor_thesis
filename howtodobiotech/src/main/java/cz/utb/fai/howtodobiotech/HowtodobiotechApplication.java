1ee4b17f26b4196e24430deba2fe59773c7ffb23
baskaklimek <baska.klimekova@gmail.com>
2023-04-05 18:30:03 +0200

    generate new project by Spring Initializr

---
+++ howtodobiotech/src/main/java/cz/utb/fai/howtodobiotech/HowtodobiotechApplication.java
@@ -0,0 +1,13 @@
+package cz.utb.fai.howtodobiotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtodobiotechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowtodobiotechApplication.class, args);
+    }
+
+}
---
+++ howtodobiotech/src/test/java/cz/utb/fai/howtodobiotech/HowtodobiotechApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.utb.fai.howtodobiotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtodobiotechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
