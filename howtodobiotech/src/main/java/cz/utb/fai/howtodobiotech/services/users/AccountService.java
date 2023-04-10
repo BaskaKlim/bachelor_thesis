5bbe57fe9f2e2be5c358e7d9afb8c2996e4d2d04
baskaklimek <baska.klimekova@gmail.com>
2023-04-10 21:46:35 +0200

    create service for Account Repository

---
+++ howtodobiotech/src/main/java/cz/utb/fai/howtodobiotech/services/users/AccountService.java
@@ -0,0 +1,58 @@
+package cz.utb.fai.howtodobiotech.services.users;
+
+import cz.utb.fai.howtodobiotech.models.users.Account;
+import cz.utb.fai.howtodobiotech.repositories.users.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Optional<Account> getAccountById(Integer id) {
+        return accountRepository.findById(id);
+    }
+
+    public List<Account> getAllAccounts() {
+        List<Account> accounts = new ArrayList<Account>();
+        accountRepository.findAll().forEach(accounts::add);
+
+        return accounts;
+    }
+
+    public Account addAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public Account updateUser(Account account) {
+        return accountRepository.save(account);
+    }
+
+    public void deleteAccountById(Integer id) {
+        accountRepository.deleteById(id);
+    }
+
+    public  Optional<Account> findByUsername(String username){
+        return accountRepository.findByUsername(username);
+
+    }
+
+    public  Boolean existsByUsername(String username){
+        return accountRepository.existsByUsername(username);
+    }
+
+    public  Boolean existsByPassword(String password){
+        return accountRepository.existsByPassword(password);
+    };
+
+}
