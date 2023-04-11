9f9f9d9f67387280018f48f8854dd93eb4b5d225
baskaklimek <baska.klimekova@gmail.com>
2023-04-11 22:14:47 +0200

    create service and API controller for Startup Opportunity

---
+++ howtodobiotech/src/main/java/cz/utb/fai/howtodobiotech/api/content/StartupOptController.java
@@ -0,0 +1,92 @@
+package cz.utb.fai.howtodobiotech.api.content;
+
+import cz.utb.fai.howtodobiotech.models.content.SkillOpt;
+import cz.utb.fai.howtodobiotech.models.content.StartupOpt;
+import cz.utb.fai.howtodobiotech.services.content.StartupOptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RequestMapping("api/startup-opportunities")
+@RestController
+public class StartupOptController {
+
+    @Autowired
+    StartupOptService startupOptService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StartupOpt> selectStartupOptById(@PathVariable("id") Integer id) {
+        Optional<StartupOpt> startupOptData = startupOptService.getStartupOptById(id);
+
+        if (startupOptData.isPresent()) {
+            return new ResponseEntity<>(startupOptData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<StartupOpt>> getAllStartupOpts() {
+        try {
+            List<StartupOpt> startupOpts = new ArrayList<>();
+
+            startupOptService.getAllStartupOpts().forEach(startupOpts::add);
+            if (startupOpts.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(startupOpts, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping()
+    public ResponseEntity<StartupOpt> createStartupOpt(@RequestBody StartupOpt startupOpt) {
+        try {
+            StartupOpt _startupOpt = startupOptService
+                    .addSStartupOpt(new StartupOpt(startupOpt.getTitle(), startupOpt.getProvider(), startupOpt.getDescription(), startupOpt.getStartDate(), startupOpt.getEndDate(), startupOpt.getWebsite(), startupOpt.getCategories(), startupOpt.getSupportCategories()));
+            return new ResponseEntity<>(_startupOpt, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Boolean> deleteStartupOpt(@PathVariable("id") Integer id) {
+        try {
+            startupOptService.deleteStartupOptById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StartupOpt> updateStartupOpt(@PathVariable("id") Integer id, @RequestBody StartupOpt startupOpt) {
+        Optional<StartupOpt> startupOptData = startupOptService.getStartupOptById(id);
+        if (startupOptData.isPresent()) {
+            StartupOpt _startupOpt = startupOptData.get();
+            _startupOpt.setTitle(startupOpt.getTitle());
+            _startupOpt.setProvider(startupOpt.getProvider());
+            _startupOpt.setDescription(startupOpt.getDescription());
+            _startupOpt.setStartDate(startupOpt.getStartDate());
+            _startupOpt.setEndDate(startupOpt.getEndDate());
+            _startupOpt.setWebsite(startupOpt.getWebsite());
+            _startupOpt.setCategories(startupOpt.getCategories());
+            _startupOpt.setSupportCategories(startupOpt.getSupportCategories());
+
+
+
+            return new ResponseEntity<>(startupOptService.updateStartupOpt(_startupOpt), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ howtodobiotech/src/main/java/cz/utb/fai/howtodobiotech/services/content/StartupOptService.java
@@ -0,0 +1,74 @@
+package cz.utb.fai.howtodobiotech.services.content;
+
+
+import cz.utb.fai.howtodobiotech.models.content.SkillOpt;
+import cz.utb.fai.howtodobiotech.models.content.StartupOpt;
+import cz.utb.fai.howtodobiotech.repositories.content.StartupOptRepository;
+import cz.utb.fai.howtodobiotech.utils.enums.EBiotechCategory;
+import cz.utb.fai.howtodobiotech.utils.enums.ESupportCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StartupOptService {
+    StartupOptRepository startupOptRepository;
+
+    @Autowired
+    public StartupOptService(StartupOptRepository startupOptRepository) {
+        this.startupOptRepository = startupOptRepository;
+    }
+
+    public Optional<StartupOpt> getStartupOptById(Integer id) {
+        return startupOptRepository.findById(id);
+    }
+
+    public List<StartupOpt> getAllStartupOpts() {
+        List<StartupOpt> startupOpts = new ArrayList<StartupOpt>();
+        startupOptRepository.findAll().forEach(startupOpts::add);
+
+        return startupOpts;
+    }
+
+
+    public StartupOpt addSStartupOpt(StartupOpt startupOpt) {
+        return startupOptRepository.save(startupOpt);
+
+    }
+
+    public StartupOpt updateStartupOpt(StartupOpt startupOpt) {
+        return startupOptRepository.save(startupOpt);
+    }
+
+    public void deleteStartupOptById(Integer id) {
+        startupOptRepository.deleteById(id);
+    }
+
+    public Optional<StartupOpt> findByCategories(EBiotechCategory biotechCategory) {
+        return startupOptRepository.findByCategories(biotechCategory);
+    }
+
+    public Optional<StartupOpt> findBySupportCategories(ESupportCategory supportCategory) {
+        return startupOptRepository.findBySupportCategories(supportCategory);
+    }
+
+    public Optional<StartupOpt> findByTitle(String title) {
+        return startupOptRepository.findByTitle(title);
+    }
+
+    public Optional<StartupOpt> findByProvider(String organizer) {
+        return startupOptRepository.findByProvider(organizer);
+    }
+
+    public Optional<StartupOpt> findByStartDate(Date startDate) {
+        return startupOptRepository.findByStartDate(startDate);
+    }
+
+    public Optional<SkillOpt> findByEndDate(Date endDate) {
+        return startupOptRepository.findByEndDate(endDate);
+    }
+}
