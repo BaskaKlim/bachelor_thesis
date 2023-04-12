a0125e567075b672164ddc26f1c52cd5e0e06361
baskaklimek <baska.klimekova@gmail.com>
2023-04-12 22:47:02 +0200

    create AccountDetailsImpl Service for security

---
+++ howtodobiotech/src/main/java/cz/utb/fai/howtodobiotech/security/services/AccountDetailsImpl.java
@@ -0,0 +1,126 @@
+package cz.utb.fai.howtodobiotech.security.services;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import cz.utb.fai.howtodobiotech.models.users.Account;
+import cz.utb.fai.howtodobiotech.models.users.Contact;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class AccountDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private String description;
+    private String url;
+    private String email;
+    private String username;
+    @JsonIgnore
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private Set<Contact> contacts = new HashSet<>();
+
+    public AccountDetailsImpl(Integer id, String name, String description, String url, String email, String username, String password, Collection<? extends GrantedAuthority> authorities, Set<Contact> contacts) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+        this.contacts = contacts;
+    }
+
+    public static AccountDetailsImpl build(Account account) {
+        List<GrantedAuthority> authorities = account.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new AccountDetailsImpl(
+                account.getId(),
+                account.getName(),
+                account.getDescription(),
+                account.getUrl(),
+                account.getEmail(),
+                account.getUsername(),
+                account.getPassword(),
+                authorities,
+                account.getContacts());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Set<Contact> getContacts() {
+        return contacts;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        AccountDetailsImpl user = (AccountDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+}
