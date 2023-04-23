3acbc52bb7527a590805362f6fd0ebef36979e95
baskaklimek <baska.klimekova@gmail.com>
2023-04-23 23:15:32 +0200

    App.js navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AddSkillOpt from "./components/add-skillOpt.component";
+import SkillOpt from "./components/skillOpt.component";
+import SkillOptList from "./components/skillOpt-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/skillOpts"} className="navbar-brand">
+            HoeToDoBiotech
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/skillOpts"} className="nav-link">
+              Skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/skillOpts"]} component={SkillOptList} />
+            <Route exact path="/add" component={AddSkillOpt} />
+            <Route path="/skillOpts/:id" component={SkillOpt} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
